227d81cdde4273d90d8dc8a53a4eff142d9b8864
Ivan <ivan.borisov@red-soft.ru>
2023-02-22 15:31:26 +0300

    commit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+<h1 align="center">Необходимо выполнить команды:</h1>
+<h3>1. rustup target add wasm32-unknown-unknown</h3>
+<h3>2. cargo install --locked trunk</h3>
---
+++ src/main.rs
@@ -0,0 +1,50 @@
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use yew::prelude::*;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    //yew::Renderer::<app>::new().render();
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+        .bind(("127.0.0.1", 7878))?
+        .run()
+        .await
+
+}
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[function_component]
+fn app() -> Html {
+    let counter = use_state(|| 0);
+    let onclick = {
+        let counter = counter.clone();
+        move |_| {
+            let value = *counter + 1;
+            counter.set(value);
+        }
+    };
+
+    html! {
+        <div>
+            <button {onclick}>{ "+1" }</button>
+            <p>{ *counter }</p>
+        </div>
+    }
+}
